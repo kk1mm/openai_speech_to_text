5565b4af5fe478c9ea4fb561ce91958bf75f3111
Myroslav <kk0001mm@gmail.com>
2024-05-10 16:33:59 +0200

    divided project for 2 files

---
+++ funcs.py
@@ -0,0 +1,23 @@
+from main import transcriber, Bot
+import io
+from aiogram.types import Voice
+from pydub import AudioSegment
+
+async def audio_to_text(file_path: str) -> str:
+    """Принимает путь к аудио файлу, возвращает текст файла."""
+
+    with open(file_path, "rb") as audio_file:
+        transcript = transcriber.transcribe(audio_file)
+    return transcript.text
+
+
+async def save_voice_as_mp3(bot: Bot, voice: Voice) -> str:
+    """Скачивает голосовое сообщение и сохраняет в формате mp3."""
+    voice_file_info = await bot.get_file(voice.file_id)
+    voice_ogg = io.BytesIO()
+    await bot.download_file(voice_file_info.file_path, voice_ogg)
+    voice_mp3_path = f"voice_files/voice-{voice.file_unique_id}.mp3"
+    AudioSegment.from_file(voice_ogg, format="ogg").export(
+        voice_mp3_path, format="mp3"
+    )
+    return voice_mp3_path
--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import logging
 #hello
 from AI_TOKEN import BOT_TOKEN, TOKEN1
 import asyncio
-import io
+
 
 from aiogram import Bot, Dispatcher, F, Router
-from aiogram.types import Message, Voice
+from aiogram.types import Message
 
-from pydub import AudioSegment
+
 
 import assemblyai as aai
 
+from funcs import *
 aai.settings.api_key = TOKEN1
 
 router: Router = Router()
@@ -19,24 +20,6 @@ config = aai.TranscriptionConfig(language_code="uk", dual_channel=True)
 transcriber = aai.Transcriber(config=config)
 
 MALYSHKA = [1,2]
-async def audio_to_text(file_path: str) -> str:
-    """Принимает путь к аудио файлу, возвращает текст файла."""
-
-    with open(file_path, "rb") as audio_file:
-        transcript = transcriber.transcribe(audio_file)
-    return transcript.text
-
-
-async def save_voice_as_mp3(bot: Bot, voice: Voice) -> str:
-    """Скачивает голосовое сообщение и сохраняет в формате mp3."""
-    voice_file_info = await bot.get_file(voice.file_id)
-    voice_ogg = io.BytesIO()
-    await bot.download_file(voice_file_info.file_path, voice_ogg)
-    voice_mp3_path = f"voice_files/voice-{voice.file_unique_id}.mp3"
-    AudioSegment.from_file(voice_ogg, format="ogg").export(
-        voice_mp3_path, format="mp3"
-    )
-    return voice_mp3_path
 
 
 @router.message(F.content_type == "voice")
