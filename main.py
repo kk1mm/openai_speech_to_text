28ef9441bd604765f04c4c063519c1b033f1e498
Myroslav <kk0001mm@gmail.com>
2024-05-10 14:52:44 +0200

    1

--- main.py
+++ main.py
@@ -13,29 +13,19 @@ import assemblyai as aai
 
 aai.settings.api_key = "7b34956ed6134014b7467c979962f2cf"
 
-
-
-
-# client = OpenAI()
-# openai.api_key = AI_TOKEN
-
-
 router: Router = Router()
 logging.basicConfig(level=logging.INFO)
 config = aai.TranscriptionConfig(language_code="uk", dual_channel=True)
 transcriber = aai.Transcriber(config=config)
 
+
 async def audio_to_text(file_path: str) -> str:
     """Принимает путь к аудио файлу, возвращает текст файла."""
-    # with open(file_path, "rb") as audio_file:
-    #     transcript = await openai.Audio.atranscribe(
-	#         "whisper-1", audio_file
-	#     )
-    # return transcript["text"]
-    # client = OpenAI()
+
     with open(file_path, "rb") as audio_file:
         transcript = transcriber.transcribe(audio_file)
     return transcript.text
+
 
 async def save_voice_as_mp3(bot: Bot, voice: Voice) -> str:
     """Скачивает голосовое сообщение и сохраняет в формате mp3."""
@@ -44,8 +34,8 @@ async def save_voice_as_mp3(bot: Bot, voice: Voice) -> str:
     await bot.download_file(voice_file_info.file_path, voice_ogg)
     voice_mp3_path = f"voice_files/voice-{voice.file_unique_id}.mp3"
     AudioSegment.from_file(voice_ogg, format="ogg").export(
-	    voice_mp3_path, format="mp3"
-	)
+        voice_mp3_path, format="mp3"
+    )
     return voice_mp3_path
 
 
@@ -62,6 +52,7 @@ async def process_voice_message(message: Message, bot: Bot):
 @router.message(F.content_type != "voice")
 async def not_voice(message: Message, bot: Bot):
     await message.reply(text='Send an audio pls 8:')
+
 
 async def main():
     bot: Bot = Bot(token=BOT_TOKEN)
